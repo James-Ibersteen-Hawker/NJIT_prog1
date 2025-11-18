ab9a5d286612ff7e2b75f403e0d2abcf14e39646
James Hawker <526rserbinenko@frhsd.com>
2025-11-18 04:46:16 +0000

    added carousel buttons

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(async () => {
   let list = await fetch("tanks.json");
   list = await list.json();
+  let dir = "next";
+  const time = 8000;
   const toID = (str) => str.replace(/[\s/]+/g, "-");
   const wait = (t) => new Promise((resolve) => setTimeout(() => resolve(), t));
   const carousel = {
@@ -13,13 +15,15 @@ $(document).ready(async () => {
       });
       this.render();
     },
-    next() {
+    async next() {
       this.i = (this.i + 1) % list.length;
       this.render();
+      dir = "next";
     },
     prev() {
       this.i = (this.i - 1 + list.length) % list.length;
       this.render();
+      dir = "prev";
     },
     async render() {
       const iP = (this.i - 1 + list.length) % list.length;
@@ -30,7 +34,7 @@ $(document).ready(async () => {
       list.forEach(({ name }) => $(`#${toID(name)}`).attr("class", "none"));
       [e1, e2, e3].forEach((e) => e.removeClass("none"));
       e1.addClass("moveIn"), e2.addClass("toCurrent"), e3.addClass("toRight"); //animations
-      await wait(1000);
+      // await wait(1000);
       e1.attr("class", "backL"), e2.attr("class", "current");
       e3.attr("class", "backR");
       const { name, blurb } = list[this.i];
@@ -38,8 +42,18 @@ $(document).ready(async () => {
     },
   };
   carousel.init();
-  //add buttons later
-  $(document).on("click", () => {
-    carousel.next();
+  $(".rArrow").on("click", () => carousel.next());
+  $(".lArrow").on("click", () => carousel.prev());
+  const intervalFunc = () =>
+    dir === "next" ? carousel.next() : carousel.prev();
+  let interval = setInterval(intervalFunc, time);
+  $(document).on("keydown", (event) => {
+    if (event.key === "Escape") clearInterval(interval);
+    else if (event.key === " ") interval = setInterval(intervalFunc, time);
+  });
+  $(".more").hide();
+  $(".moreBtn").on("click", () => {
+    $(".moreBtn").toggleClass("rot90");
+    $(".more").toggle();
   });
 });
