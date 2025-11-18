1906b1f8bc000f356efc8056dc89d9502eeb6799
James Hawker <526rserbinenko@frhsd.com>
2025-11-18 13:19:32 +0000

    More small tweaks, user interface improvement

--- script.js
+++ script.js
@@ -16,14 +16,22 @@ $(document).ready(async () => {
       this.render();
     },
     async next() {
+      $(".moreBtn").removeClass("rot90");
+      $(".more").addClass("gone");
       this.i = (this.i + 1) % list.length;
       this.render();
       dir = "next";
+      $(".img").attr("style", "pointer-events:none;");
+      setTimeout(() => $(".img").attr("style", "pointer-events:auto;"), 900);
     },
     prev() {
+      $(".moreBtn").removeClass("rot90");
+      $(".more").addClass("gone");
       this.i = (this.i - 1 + list.length) % list.length;
       this.render();
       dir = "prev";
+      $(".img").attr("style", "pointer-events:none;");
+      setTimeout(() => $(".img").attr("style", "pointer-events:auto;"), 900);
     },
     async render() {
       const iP = (this.i - 1 + list.length) % list.length;
@@ -34,7 +42,6 @@ $(document).ready(async () => {
       list.forEach(({ name }) => $(`#${toID(name)}`).attr("class", "none"));
       [e1, e2, e3].forEach((e) => e.removeClass("none"));
       e1.addClass("moveIn"), e2.addClass("toCurrent"), e3.addClass("toRight"); //animations
-      // await wait(1000);
       e1.attr("class", "backL"), e2.attr("class", "current");
       e3.attr("class", "backR");
       const { name, blurb } = list[this.i];
@@ -44,12 +51,12 @@ $(document).ready(async () => {
   carousel.init();
   $(".rArrow").on("click", () => carousel.next());
   $(".lArrow").on("click", () => carousel.prev());
+  $(".img").on("click", ".backR", () => carousel.next());
+  $(".img").on("click", ".backL", () => carousel.prev());
   const intervalFunc = async () => {
     $(".moreBtn").removeClass("rot90");
     $(".more").addClass("gone");
     dir === "next" ? carousel.next() : carousel.prev();
-    $(".moreBtn").addClass("rot90");
-    $(".more").removeClass("gone");
   };
   let interval = setInterval(intervalFunc, time);
   $(document).on("keydown", (event) => {
